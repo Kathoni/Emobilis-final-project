5c022e7714528cc1415da24837532592896f2ede
Kathoni <thrusherkeys@gmail.com>
2024-11-28 14:27:44 +0300

    updating models.py

--- Community/application/models.py
+++ Community/application/models.py
@@ -1,4 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Citizen(models.Model):
+    image = models.ImageField(upload_to='citizen_images/', blank=True)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
+
+class Login(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Signing(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
